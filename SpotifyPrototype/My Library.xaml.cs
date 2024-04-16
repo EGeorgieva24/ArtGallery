d78796942709c63502832e5b6c454af672c190ea
e.georgieva24 <e.georgieva24@LAB108PC13>
2024-04-16 15:25:41 +0300

    Add project files.

---
+++ SpotifyPrototype/My Library.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SpotifyPrototype
+{
+    /// <summary>
+    /// Interaction logic for My_Library.xaml
+    /// </summary>
+    public partial class My_Library : Window
+    {
+        public My_Library()
+        {
+            InitializeComponent();
+        }
+    }
+}
